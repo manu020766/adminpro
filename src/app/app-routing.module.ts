10bf22d46390e5e7073487874c2ca1d7a77b171e
manu020766 <manu020766@yahoo.es>
2020-02-03 16:40:47 +0100

    add register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,16 @@ import { Routes, RouterModule } from '@angular/router'
 
 import { PagesComponent } from './pages/pages.component'
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component'
 import { LoginComponent } from './login/login.component'
+import { RegisterComponent } from './login/register.component'
+import { DashboardComponent } from './pages/dashboard/dashboard.component'
 import { ProgressComponent } from './pages/progress/progress.component'
 import { Graficas1Component } from './pages/graficas1/graficas1.component'
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component'
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   {
     path: '',
     component: PagesComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component'
 import { LoginComponent } from './login/login.component'
+import { RegisterComponent } from './login/register.component'
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component'
 import { DashboardComponent } from './pages/dashboard/dashboard.component'
 import { ProgressComponent } from './pages/progress/progress.component'
 import { Graficas1Component } from './pages/graficas1/graficas1.component'
 import { HeaderComponent } from './shared/header/header.component'
 import { SidebarComponent } from './shared/sidebar/sidebar.component'
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component'
 import { PagesComponent } from './pages/pages.component'
 
 @NgModule({
@@ -24,7 +25,8 @@ import { PagesComponent } from './pages/pages.component'
     HeaderComponent,
     SidebarComponent,
     BreadcrumbsComponent,
-    PagesComponent
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
