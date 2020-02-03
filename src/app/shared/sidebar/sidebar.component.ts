c215fc2f6d53fa90e8d9bee2bc8f8f5af781c0a2
manu020766 <manu020766@yahoo.es>
2020-02-03 12:30:59 +0100

    add header and sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: []
+  styleUrls: []
 })
 export class SidebarComponent implements OnInit {
 
