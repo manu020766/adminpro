3dcd2b46d11fe3e459a3f492fc3926d4c85c484e
manu020766 <manu020766@yahoo.es>
2020-02-04 15:48:05 +0100

    fix and inprove incrementador component

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core'
+import { Component, Output, EventEmitter, Input, ViewChild, ElementRef } from '@angular/core'
 
 @Component({
   selector: 'app-incrementador',
@@ -6,6 +6,8 @@ import { Component, Output, EventEmitter, Input } from '@angular/core'
   styles: []
 })
 export class IncrementadorComponent {
+  @ViewChild('txtProgress', {static:false}) txtProgress: ElementRef
+
   @Input() leyenda:string = 'leyenda'
   @Output() porcentajeActual = new EventEmitter()
 
@@ -20,6 +22,9 @@ export class IncrementadorComponent {
     if (this.porcentaje > 100) this.porcentaje = 100
     if (this.porcentaje < 0) this.porcentaje = 0
 
+    this.txtProgress.nativeElement.value = +this.porcentaje
+
     this.porcentajeActual.emit(this.porcentaje)
+    this.txtProgress.nativeElement.focus()
   }
 }
