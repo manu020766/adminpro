cbd5a1ec16c9d175c25b7b7b6834b4c3c954dbea
manu020766 <manu020766@yahoo.es>
2020-02-07 11:02:56 +0100

    add metadata info title y description

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators'
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -9,7 +10,9 @@ import { filter, map } from 'rxjs/operators'
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor(public router:Router) { }
+  constructor(private router:Router,
+              private title: Title,
+              private meta: Meta ) { }
 
   titulo: string
   ngOnInit() {
@@ -19,6 +22,16 @@ export class BreadcrumbsComponent implements OnInit {
       filter((event:ActivationEnd) => event.snapshot.firstChild === null ),  // concretamente en el evento que tiene: firstChild === null
       map((info:ActivationEnd) => info.snapshot.data)                        // estraigo la info de la data en este caso: Objeto data
     )
-    .subscribe(dataEnLaUrlRouter => this.titulo = dataEnLaUrlRouter.titulo )   // Obtengo el titulo del objeto data
+    .subscribe(dataEnLaUrlRouter => {
+      this.titulo = dataEnLaUrlRouter.titulo // Obtengo el titulo del objeto data
+      this.title.setTitle(this.titulo)       // Establezco el titulo de la metadata de la página
+
+      const metaTag:MetaDefinition = {
+        nombre: 'description',
+        content: 'Muetra información: ' + this.titulo
+      }
+
+      this.meta.updateTag(metaTag)  // Añado a la pagina el meta decription
+    })
   }
-}
+} 
