d4d3efe7487f97c93d7f314452f71ba9f424169f
manu020766 <manu020766@yahoo.es>
2020-02-21 12:01:50 +0100

    form login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router'
+import { Usuario } from '../models/usuario.model'
+import { FormGroup } from '@angular/forms';
 
 declare function init_plugins()
 
@@ -10,14 +12,39 @@ declare function init_plugins()
 })
 export class LoginComponent implements OnInit {
 
+  // @ViewChild('usuarioForm', { static:false }) form:any //Esta es una forma de hacer referencia al formulario
+
+  usuario:Usuario
+  recuerdame:boolean
   constructor(public router:Router) { }
 
   ngOnInit() {
     init_plugins()
+    
+    this.usuario = new Usuario('','','')
+    this.recuerdame = false
   }
 
-  ingresar() {
-    console.log('hola')
-    this.router.navigate(['dashboard'])
+  // ingresar() {                                       // De esta forma funcionaria con la referencia al viewChild
+  //   console.log('Formulario: ', this.form.valid)
+  //   console.log('Usuario: ',this.usuario)
+  //   console.log('Recuerdame: ', this.recuerdame)
+
+  //   if (this.form.invalid) {
+  //     this.form.controls['email'].markAsTouched()
+  //     this.form.controls['password'].markAsTouched()
+  //   } else this.router.navigate(['dashboard'])
+  // }
+
+  ingresar(usuarioForm:FormGroup) {
+    console.log('Formulario: ', usuarioForm.valid)
+    console.log('Usuario: ',this.usuario)
+    console.log('Recuerdame: ', this.recuerdame)
+
+    if (usuarioForm.invalid) {
+      usuarioForm.controls['email'].markAsTouched()
+      usuarioForm.controls['password'].markAsTouched()
+    } else this.router.navigate(['dashboard'])
   }
+
 }
