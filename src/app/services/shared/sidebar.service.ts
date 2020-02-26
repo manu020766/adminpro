f6bc87e7b087cf741d9dac1ffc520e4f385226ad
manu020766 <manu020766@yahoo.es>
2020-02-26 10:40:22 +0100

    new menu in the sidebar

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -13,6 +13,15 @@ export class SidebarService {
         { titulo: 'ProgressBar', url: '/progress' },
         { titulo: 'Gráficas', url: '/graficas1' }
       ]
+    },
+    {
+      titulo: 'Mantenimientos',
+      icono: 'mdi mdi-folder-lock-open',
+      submenu: [
+        { titulo: 'Usuarios', url: '/usuarios' },
+        { titulo: 'Hospitales', url: '/hospitales' },
+        { titulo: 'Médicos', url: '/medicos' }
+      ]
     }
   ]
 
